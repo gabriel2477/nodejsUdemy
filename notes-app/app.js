a70fd191dfd305687032232df510fad153720785
Gabriel Villalba <gvillalba@pkglobal.com>
2021-11-17 15:24:07 -0300

    Agrega modulo chalk y realiza algunas pruebas agregando colores a los mensajes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const notes = require('./notes.js');
+const chalk = require('chalk');
+
 fs.writeFileSync('notes.txt','Hello this is my first document writen with NodeJs');
 fs.appendFileSync('notes.txt','I live in Posadas'); //Para agregar otro mensaje al documento anterior.
 
@@ -8,3 +10,6 @@ fs.appendFileSync('notes.txt','I live in Posadas'); //Para agregar otro mensaje 
 
 const sum = notes(4,7);
 console.log(sum);
+console.log(chalk.blue(sum))
+console.log(chalk.underline.bold(sum))
+console.log(chalk.bgGreen(sum))
