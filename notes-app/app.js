1efa96710015e886257bf8baf7d5dbb8072bf743
Gabriel Villalba <gvillalba@pkglobal.com>
2021-11-17 12:45:15 -0300

    Agrega linea de texto con appendFileSync

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
 
 fs.writeFileSync('notes.txt','Hello this is my first document writen with NodeJs');
+fs.appendFileSync('notes.txt','I live in Posadas'); //Para agregar otro mensaje al documento anterior.
