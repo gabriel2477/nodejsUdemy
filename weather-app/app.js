8d6cf7835d1955346bedcba1fa007ce6584f65cf
Gabriel Villalba <gvillalba@pkglobal.com>
2021-11-29 12:45:50 -0300

    Handling errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -15,7 +15,16 @@ const request = require('postman-request')
 const urlGeoLocation = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Posadas.json?access_token=pk.eyJ1IjoiZ2FicmllbDI0NzciLCJhIjoiY2t3a3FmcXFhMXV5bDJ1cGF0bnA1ODlyMCJ9.VJ9xanD1n2gHfFUvPZuQPw&limit=1'
 
 request({url: urlGeoLocation, json:true}, (error, response, body) => {
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
-    console.log('La longitud de Posadas es ' + longitude + '. Y su latitud es ' + latitude)
+    
+    if(error){
+        console.log('Unable to conect to location services!')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search.')
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log('La longitud de Posadas es ' + longitude + '. Y su latitud es ' + latitude)
+    }
+
+    
 })
