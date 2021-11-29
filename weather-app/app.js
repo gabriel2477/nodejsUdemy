07a93eea1a513a8415748c3c53a59b8e207450ae
Gabriel Villalba <gvillalba@pkglobal.com>
2021-11-29 10:40:09 -0300

    Use json option instead of JSON.parse

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,7 @@ const request = require('postman-request')
 
 const urlWeather = 'http://api.weatherstack.com/current?access_key=3c59f916cfe35ce52716359d524f2a33&query=Posadas,%20Misiones,%20Argentina'
 
-request({url: urlWeather},(error,response,body)=>{
-    const data = JSON.parse(response.body)
+request({url: urlWeather, json: true},(error,response,body)=>{
+    const data = response.body
     console.log('En '+ data.location.name + ' hace ' + data.current.temperature + '. Con una humedad de ' + data.current.humidity)
 })
